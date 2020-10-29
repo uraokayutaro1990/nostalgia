79afb616dcf51813508dbecff5b881a0d5277964
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-10-29 14:24:56 +0900

    編集機能コントローラーとビュー、削除機能のコントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,16 +12,26 @@ class ItemsController < ApplicationController
   def create
     Item create(item_params)
     #	アイテムモデル保存する
+    # praivateでitem_paramsで引用してくるものを指定
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
   end
 
   def edit
     @item = Item.find(params[:id])
-    
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :image)
+    params.require(:item).permit(:name, :image, :text)
     #アイテムの名前と写真だけを許可
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :items, only: [:index, :new ,:create ,:destroy ,:edit]
+  resources :items, only: [:index, :new ,:create ,:destroy ,:edit ,:update]
   # newアクション生存を追加
 end
 
