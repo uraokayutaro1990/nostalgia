5a5367e62f9411131cf882a459c493d8514b2678
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-11-11 15:42:08 +0900

    SyntaxError修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,5 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.248.24.69', user: 'user: 'ec2-user', roles: %w{app db web}
+server '54.248.24.69', user: 'ec2-user', roles: %w{app db web}
+	
